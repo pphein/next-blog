a852866d2ca01a3bce6daa636e1a88286636abb9
pphein <pph.hein87@gmail.com>
2024-02-07 06:26:41 +0630

    initial

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link"
+import { FaYoutube, FaTwitter, FaGithub, FaFacebook } from "react-icons/fa"
+
+export default function Navbar() {
+    return (
+        <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
+            <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
+                <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
+                    <Link
+                        href="/"
+                        className="text-white/90 no-underline hver:text-white">
+                        Pyae Phyo Hein
+                    </Link>
+                </h1>
+                <div className="flex flex-row justify-center sm:justify-evently align-middle gap-4 text-white text-4xl lg:text-5xl">
+                    <Link
+                        className="text-white/90 hover:text-white"
+                        href="https://www.facebook.com/pph.hein"
+                    >
+                        <FaFacebook />
+                    </Link>
+                    <Link
+                        className="text-white/90 hover:text-white"
+                        href="https://www.github.com/pphein"
+                    >
+                        <FaGithub />
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    )
+}
